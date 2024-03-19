e763ba72ba41947a21e6343b24733ce60b1c0d7a
fferrlop <fferrlop@myuax.com>
2024-03-19 23:13:59 +0100

    ambiente

---
+++ src/main/java/Entorno/Ambiente.java
@@ -0,0 +1,13 @@
+package Entorno;
+
+import Organismos.Organismo;
+import java.util.List;
+
+public class Ambiente {
+    private Clima clima;
+    private Terreno terreno;
+    private Recursos recursos;
+    private List<Organismo> organismos;
+
+
+}
