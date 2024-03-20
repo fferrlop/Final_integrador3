6a5880ff18cefee693caa4aa27a0bfdd063650f6
fferrlop <fferrlop@myuax.com>
2024-03-20 19:05:06 +0100

    lagarto

--- src/main/java/Organismos/Lagarto.java
+++ src/main/java/Organismos/Lagarto.java
@@ -1,17 +1,23 @@
 package Organismos;
 
 public class Lagarto extends Organismo {
-    public Lagarto (Posicion posicion, int salud, int edad, boolean estadoReproductivo) {
+    public Lagarto(Posicion posicion, int salud, int edad, boolean estadoReproductivo) {
         super(posicion, salud, edad, estadoReproductivo);
     }
+
     @Override
     public void interactuar(Organismo otroOrganismo) {
-        if (otroOrganismo instanceof Lagarto) {
-            this.salud += 5;
+        if (otroOrganismo instanceof Abeja) {
+            otroOrganismo.salud = 0;
+            otroOrganismo.vivo = false; // La abeja "desaparece"
+            System.out.println("El lagarto ha cazado a la abeja");
         }
     }
 
-    public int getSalud() {
-        return this.salud;
+    @Override
+    public Organismo reproducirse() {
+
+        System.out.println("El lagarto se está reproduciendo");
+        return new Lagarto(new Posicion(0, 0), 100, 0, true);
     }
 }
