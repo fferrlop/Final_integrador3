d387cf7336e0c7f6e28abe4de1a60788e30ce579
fferrlop <fferrlop@myuax.com>
2024-03-20 19:05:29 +0100

    organismo

--- src/main/java/Organismos/Organismo.java
+++ src/main/java/Organismos/Organismo.java
@@ -5,15 +5,34 @@ public abstract class Organismo {
     protected int salud;
     protected int edad;
     protected boolean estadoReproductivo;
+    protected boolean vivo; // Nuevo campo
 
     public Organismo(Posicion posicion, int salud, int edad, boolean estadoReproductivo) {
         this.posicion = posicion;
         this.salud = salud;
         this.edad = edad;
         this.estadoReproductivo = estadoReproductivo;
+        this.vivo = true; // Todos los organismos comienzan vivos
     }
 
     public abstract void interactuar(Organismo otroOrganismo);
+
+
+
+    public int getSalud() {
+        return this.salud;
+    }
+
+    public boolean estaVivo() { // Nuevo método
+        return this.vivo;
+    }
+
+    public void crecer() {
+        this.edad++;
+        // Incrementar la salud si es necesario
+    }
+
+    public abstract Organismo reproducirse();
 }
 
 
